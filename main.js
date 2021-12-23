8f67dec69b584308ac970a3ba2409d8059a15c9c
DPaul <36795646+DPaul-sys@users.noreply.github.com>
2021-12-23 17:41:02 +0100

    WebGL Hello Black Canvas implementation

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-alert('Hello, World!');
+function main() {
+    const canvas = document.querySelector("#glCanvas");
+    //init GL context
+    const gl = canvas.getContext("webgl");
+
+    //check if GL initialized
+    if(gl == null) {
+        alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+        return;
+    }
+
+    //fill screen, black, full opacity
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    //clear buffers to re-render
+    gl.clear(gl.COLOR_BUFFER_BIT)
+}
+
+window.onload = main;
